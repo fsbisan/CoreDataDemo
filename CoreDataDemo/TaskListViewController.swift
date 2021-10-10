9a102b4e752883cd55d91df800fe060a51924b94
Alexander <fsbisan@mail.ru>
2021-10-10 18:01:13 +0500

    All is donegfhfh

--- CoreDataDemo/TaskListViewController.swift
+++ CoreDataDemo/TaskListViewController.swift
@@ -22,7 +22,7 @@ class TaskListViewController: UITableViewController {
             self.taskList = tasks
         }
     }
-// Repositori test
+// Repositori test 2
     // MARK: - Private Methods
     private func setupNavigationBar() {
         title = "Task List"
